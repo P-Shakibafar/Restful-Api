b2992d7a26e9d4caad9f60c357e6065deb5f3f5e
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2018-06-02 19:45:05 +0430

    Create Model,Controller,Migration,Factory

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,85 @@
+<?php
+	
+	namespace App\Http\Controllers;
+	
+	use App\Model\Review;
+	use Illuminate\Http\Request;
+	
+	class ReviewController extends Controller {
+		
+		/**
+		 * Display a listing of the resource.
+		 *
+		 * @return void
+		 */
+		public function index()
+		{
+			//
+		}
+		
+		/**
+		 * Show the form for creating a new resource.
+		 *
+		 * @return void
+		 */
+		public function create()
+		{
+			//
+		}
+		
+		/**
+		 * Store a newly created resource in storage.
+		 *
+		 * @param  \Illuminate\Http\Request $request
+		 * @return void
+		 */
+		public function store(Request $request)
+		{
+			//
+		}
+		
+		/**
+		 * Display the specified resource.
+		 *
+		 * @param  \App\Model\Review $review
+		 * @return void
+		 */
+		public function show(Review $review)
+		{
+			//
+		}
+		
+		/**
+		 * Show the form for editing the specified resource.
+		 *
+		 * @param  \App\Model\Review $review
+		 * @return void
+		 */
+		public function edit(Review $review)
+		{
+			//
+		}
+		
+		/**
+		 * Update the specified resource in storage.
+		 *
+		 * @param  \Illuminate\Http\Request $request
+		 * @param  \App\Model\Review        $review
+		 * @return void
+		 */
+		public function update(Request $request, Review $review)
+		{
+			//
+		}
+		
+		/**
+		 * Remove the specified resource from storage.
+		 *
+		 * @param  \App\Model\Review $review
+		 * @return void
+		 */
+		public function destroy(Review $review)
+		{
+			//
+		}
+	}
