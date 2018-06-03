f0e08c96d395435a83d29b48e68188e36b6adb42
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2018-06-03 11:51:31 +0430

    Relationship for Product and Review

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -1,10 +1,13 @@
 <?php
-
-namespace App\Model;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Product extends Model
-{
-    //
-}
+	
+	namespace App\Model;
+	
+	use Illuminate\Database\Eloquent\Model;
+	
+	class Product extends Model {
+		
+		public function reviews()
+		{
+			return $this -> hasMany(Review::class);
+		}
+	}
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -1,10 +1,13 @@
 <?php
-
-namespace App\Model;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Review extends Model
-{
-    //
-}
+	
+	namespace App\Model;
+	
+	use Illuminate\Database\Eloquent\Model;
+	
+	class Review extends Model {
+		
+		public function product()
+		{
+			return $this -> belongsTo(Product::class);
+		}
+	}
