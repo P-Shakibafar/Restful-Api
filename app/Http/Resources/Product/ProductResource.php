f3684526879eee6d4c5926132acb8b8f73a780cf
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2018-06-03 15:29:53 +0430

    Modified Product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -3,7 +3,17 @@
 	namespace App\Http\Resources\Product;
 	
 	use Illuminate\Http\Resources\Json\JsonResource;
+	use function round;
+	use function route;
 	
+	/**
+	 * @property mixed name
+	 * @property mixed details
+	 * @property mixed reviews
+	 * @property mixed discount
+	 * @property mixed price
+	 * @property mixed stock
+	 */
 	class ProductResource extends JsonResource {
 		
 		/**
@@ -20,6 +30,15 @@
 					'price'       => $this -> price,
 					'stock'       => $this -> stock,
 					'discount'    => $this -> discount,
+					'totalPrice'  => round((1 - ($this -> discount / 100)) *
+					                       $this -> price, 2),
+					'rating'      => $this -> reviews -> count() > 0 ?
+							round($this -> reviews -> sum('star') /
+							      $this -> reviews -> count(), 2) :
+							'No rating yet',
+					'href'        => [
+							'reviews' => route('reviews.index', $this -> id),
+					],
 			];
 		}
 	}
