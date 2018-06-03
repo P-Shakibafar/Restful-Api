a889e17284a2ec361b05639fe9eeac386bec3c93
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2018-06-03 13:21:09 +0430

    Product Resource created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,65 +1,65 @@
 <?php
-
-namespace App\Http\Controllers;
-
-use App\Model\Product;
-use Illuminate\Http\Request;
-
-class ProductController extends Controller
-{
 	
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return void
-	 */
-    public function index()
-    {
-        //
-    }
+	namespace App\Http\Controllers;
 	
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @return void
-	 */
-    public function store(Request $request)
-    {
-        //
-    }
+	use App\Http\Resources\Product\ProductResource;
+	use App\Model\Product;
+	use Illuminate\Http\Request;
 	
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  \App\Model\Product $product
-	 * @return void
-	 */
-    public function show(Product $product)
-    {
-        //
-    }
-	
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @param  \App\Model\Product       $product
-	 * @return void
-	 */
-    public function update(Request $request, Product $product)
-    {
-        //
-    }
-	
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  \App\Model\Product $product
-	 * @return void
-	 */
-    public function destroy(Product $product)
-    {
-        //
-    }
-}
+	class ProductController extends Controller {
+		
+		/**
+		 * Display a listing of the resource.
+		 *
+		 * @return \App\Model\Product[]|\Illuminate\Database\Eloquent\Collection
+		 */
+		public function index()
+		{
+			return Product ::all();
+		}
+		
+		/**
+		 * Store a newly created resource in storage.
+		 *
+		 * @param  \Illuminate\Http\Request $request
+		 * @return void
+		 */
+		public function store(Request $request)
+		{
+			//
+		}
+		
+		/**
+		 * Display the specified resource.
+		 *
+		 * @param  \App\Model\Product $product
+		 * @return \App\Http\Resources\Product\ProductResource
+		 */
+		public function show(Product $product): ProductResource
+		{
+			return new ProductResource($product);
+		}
+		
+		/**
+		 * Update the specified resource in storage.
+		 *
+		 * @param  \Illuminate\Http\Request $request
+		 * @param  \App\Model\Product       $product
+		 * @return void
+		 */
+		public function update(Request $request, Product $product)
+		{
+			//
+		}
+		
+		/**
+		 * Remove the specified resource from storage.
+		 *
+		 * @param  \App\Model\Product $product
+		 * @return void
+		 */
+		public function destroy(Product $product)
+		{
+			//
+		}
+	}
