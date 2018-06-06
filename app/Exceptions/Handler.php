b33729a590826116000608a3409aae8ce1953595
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2018-06-06 11:16:32 +0430

    Handels the exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,51 +1,69 @@
 <?php
-
-namespace App\Exceptions;
-
-use Exception;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-
-class Handler extends ExceptionHandler
-{
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
-    }
-}
+	
+	namespace App\Exceptions;
+	
+	use function dd;
+	use Exception;
+	use Illuminate\Database\Eloquent\ModelNotFoundException;
+	use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+	use function response;
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+	
+	class Handler extends ExceptionHandler {
+		
+		/**
+		 * A list of the exception types that are not reported.
+		 *
+		 * @var array
+		 */
+		protected $dontReport = [
+			//
+		];
+		
+		/**
+		 * A list of the inputs that are never flashed for validation exceptions.
+		 *
+		 * @var array
+		 */
+		protected $dontFlash = [
+				'password',
+				'password_confirmation',
+		];
+		
+		/**
+		 * Report or log an exception.
+		 *
+		 * @param  \Exception $exception
+		 * @return void
+		 */
+		public function report(Exception $exception)
+		{
+			parent ::report($exception);
+		}
+		
+		/**
+		 * Render an exception into an HTTP response.
+		 *
+		 * @param  \Illuminate\Http\Request $request
+		 * @param  \Exception               $exception
+		 * @return \Symfony\Component\HttpFoundation\Response
+		 */
+		public function render($request, Exception $exception)
+		{
+			if($request -> expectsJson()) {
+				if($exception instanceof ModelNotFoundException) {
+					return response() -> json([
+							'errors' => 'Model not found',
+					], Response::HTTP_NOT_FOUND);
+				}
+				if($exception instanceof NotFoundHttpException) {
+					return response() -> json([
+							'errors' => 'Incorect route',
+					], Response::HTTP_NOT_FOUND);
+				}
+			}
+			
+			return parent ::render($request, $exception);
+		}
+	}
